f30dbf249218d39a2ab9b88f684c3d35fc5ed211
Aleksandr Poltorak <sanpoltorak88@gmail.com>
2019-05-12 12:19:55 +0300

    add new project "casinoNor" (gulp)

---
+++ Nor-casino/my_project/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+browserSync = require('browser-sync').create();
+
+gulp.task('sass', function(){
+	return gulp.src('app/scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function(){
+	browserSync.init({
+	server:	{
+		baseDir:"app"
+	},
+	notify: false
+	});
+
+});
+
+gulp.task('code', function(){
+	return gulp.src('app/*.html')	
+	.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task ('watch', function(){
+	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
+	gulp.watch('app/*.html',gulp.parallel('code'))
+});
+
+gulp.task('default',gulp.parallel('watch','sass','browser-sync'));
+
+
+
+
+
