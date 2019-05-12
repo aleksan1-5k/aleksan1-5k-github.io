642cf8511bcb106d553fe59b0b586b1a52cd66b7
Aleksandr Poltorak <sanpoltorak88@gmail.com>
2019-05-12 12:02:36 +0300

    new version
    Добавлен простой адаптив

---
+++ Startuplanding/js/common.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+	$(".main_menu") .click(function(){
+		$(".menu") .slideToggle();
+	});
+	//Навигация по Landing Page
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
+	$(".menu").navigation();
+
+	//Добавляет классы дочерним блокам .block для анимации
+	//Документация: http://imakewebthings.com/jquery-waypoints/
+	$(".block").waypoint(function(direction) {
+		if (direction === "down") {
+			$(".class").addClass("active");
+		} else if (direction === "up") {
+			$(".class").removeClass("deactive");
+		};
+	}, {offset: 100});
+
+	//Плавный скролл до блока .div по клику на .scroll
+	//Документация: https://github.com/flesler/jquery.scrollTo
+	$("a.scroll").click(function() {
+		$.scrollTo($(".div"), 800, {
+			offset: -90
+		});
+	});
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 1
+	});
+	
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
+
+	//Кнопка "Наверх"
+	//Документация:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+	
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+});
